a441c3619961fe41548ff66845d25b3a789f2882
BridgitKirika <kirikabetty@gmail.com>
2022-06-03 14:37:01 +0300

    added phone numbe field in models.py

--- wepapp/models.py
+++ wepapp/models.py
@@ -5,6 +5,8 @@ class employees(models.Model):
     firstname = models.CharField(max_length =30)
     lastname = models.CharField(max_length =30)
     emp_id = models.IntegerField()
+    phone_number = models.CharField(max_length=14, blank=True)
+
 
     def _str_(self):
      return self.firstname
